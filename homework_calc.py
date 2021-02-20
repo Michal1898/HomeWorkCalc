7f4b3ff1ad0df89ca9f2b74d27346f1ecd20d5fd
Michal1898 <michal.volf@gmail.com>
2021-02-20 08:24:30 +0100

    Revize kodu linderem. Opravena podmínka ukončení programu. math_op přejmenován na math_oper. Na funkci nemá vliv.

--- homework_calc.py
+++ homework_calc.py
@@ -1,62 +1,65 @@
-import requests
-from threading import Thread, active_count
-from json import loads
 import re
+from threading import Thread, active_count
 
-results={}
-def send_calculation (operation,
-                      first_number,
-                      second_number,
-                      identifier):
+import requests
 
-    if (operation=="add"):
-        math_op="+"
-    elif (operation=="sub"):
-        math_op="-"
+results = {}
+
+
+def send_calculation(operation,
+                     first_number,
+                     second_number,
+                     identifier):
+    if operation == "add":
+        math_oper = "+"
+    elif operation == "sub":
+        math_oper = "-"
     else:
-        math_op = "*"
-    print(f"Thread {identifier} :  {first_number}{math_op}{second_number} byla poslana na server.")
+        math_oper = "*"
+    print(f"Thread {identifier} :  {first_number}{math_oper}{second_number} byla poslana na server.")
     print("Cekani na server.")
 
-    server_path="https://calc.wmwmw.cz/api/"+operation+"/"
-    result=requests.post(server_path, json={"first": first_number, "second": second_number}).json()
-    result=result["result"]
-    results[identifier]=result
-    print((f"Vysledek {identifier} je {result}"))
+    server_path = "https://calc.wmwmw.cz/api/" + operation + "/"
+    result = requests.post(server_path, json={"first": first_number, "second": second_number}).json()
+    result = result["result"]
+    results[identifier] = result
+    print(f"Vysledek {identifier} je {result}")
+
 
 if __name__ == "__main__":
-    thread_no=0
-    count_methods= {"add","sub","mul"}
-    program_end=False
+    thread_no = 0
+    count_methods = {"add", "sub", "mul"}
+    program_end = False
     while not program_end:
-        repeat_fun_insertion=True
+        repeat_fun_insertion = True
         while repeat_fun_insertion:
             inserted_string = input("Zadej: typ operace,a,b ")
             pattern = r"([a-z]{3})\,([+]+\d+|[-]+\d+|\d+)\,([+]+\d+|[-]+\d+|\d+)"
             match = re.fullmatch(pattern, inserted_string)
 
             if match:
-                repeat_fun_insertion=True
-                math_op=match.group(1)
-                if math_op in count_methods:
-                    a=int(match.group(2))
-                    b=int(match.group(3))
+                repeat_fun_insertion = True
+                math_oper = match.group(1)
+                if math_oper in count_methods:
+                    a = int(match.group(2))
+                    b = int(match.group(3))
                     thread_no += 1
-                    repeat_fun_insertion=False
+                    repeat_fun_insertion = False
 
-        thread_ident="#"+str(thread_no)
+        thread_ident = "#" + str(thread_no)
         print(thread_ident)
-        Thread(target=send_calculation, args=(math_op, a, b, thread_ident)).start()
+        # noinspection SyntaxError
+        Thread(target=send_calculation, args=(math_oper, a, b, thread_ident)).start()
 
-        program_end=input("Ukoncit program?").lower()
-        if program_end=="ano" or program_end=="a":
-            program_end==True
+        program_end = input("Ukoncit program?").lower()
+        if program_end == "ano" or program_end == "a":
+            program_end = True
         else:
-            program_end=False
+            program_end = False
 
     print("Program bude ukoncen.")
     print("Ukoncuji se thready.")
-    while(active_count()>1):
+    while active_count() > 1:
         pass
     print("Vsechny thready ukonceny.")
     print(results)
